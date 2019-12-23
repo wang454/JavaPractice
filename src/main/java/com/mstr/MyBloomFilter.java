827e74bb566fbe4871817144af0e79016c4d45b7
wangchao <wangchaozju@aliyun.com>
2019-12-24 00:34:46 +0800

    Java; config maven and use bloomfilter from guava

--- src/main/java/com/mstr/MyBloomFilter.java
+++ src/main/java/com/mstr/MyBloomFilter.java
@@ -1,6 +1,8 @@
 package com.mstr;
 
 import java.util.BitSet;
+import com.google.common.hash.BloomFilter;
+import com.google.common.hash.Funnels;
 
 public class MyBloomFilter {
 
@@ -95,5 +97,19 @@ public class MyBloomFilter {
         System.out.println(filter.contains(value3));
         System.out.println(filter.contains(value4));
 
+
+        // 创建布隆过滤器对象
+        BloomFilter<Integer> filter2 = BloomFilter.create(
+                Funnels.integerFunnel(),
+                1500,
+                0.01);
+        // 判断指定元素是否存在
+        System.out.println(filter2.mightContain(1));
+        System.out.println(filter2.mightContain(2));
+        // 将元素添加进布隆过滤器
+        filter2.put(1);
+        filter2.put(2);
+        System.out.println(filter2.mightContain(1));
+        System.out.println(filter2.mightContain(2));
     }
 }
